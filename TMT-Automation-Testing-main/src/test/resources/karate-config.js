fadaa5e89ec54b987f9403fcf55aee195f7c6f11
Bala <balakiru0601@gmail.com>
2024-09-18 18:23:00 +0530

    success

---
+++ TMT-Automation-Testing-main/src/test/resources/karate-config.js
@@ -0,0 +1,10 @@
+function fn() {
+    var config = {
+        ProductUrl: 'https://fakestoreapi.com/products'
+        CartUrl: 'https://fakestoreapi.com/carts'
+        UserUrl: 'https://fakestoreapi.com/users'
+        LoginUrl: 'https://fakestoreapi.com/auth/login'
+
+    };
+    return config;
+}
